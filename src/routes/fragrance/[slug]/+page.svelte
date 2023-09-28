e937467a239f299873c84c87edaccbcfa942ef1d
mannymontales <emmanmontales@gmail.com>
2023-09-28 11:27:57 -0400

    display fragrance

--- src/routes/fragrance/[slug]/+page.svelte
+++ src/routes/fragrance/[slug]/+page.svelte
@@ -14,12 +14,12 @@
 <h1> { data.item.id } </h1>
 <h1> { data.item.brand } </h1>
 <h1> { data.item.image_url } </h1>
-<h1> { data.item.notes } </h1>
 
+<img src="/images/fragrance_images/acqua_di_gio.jpg" alt="My Image" />
 <h1> Notes: </h1>
 <u1>
     {#each data.item.notes as note (note.id) }
-        <li>{ note.scent.name }</li>
+        <li>{ note.scent.name } { note.noteType}</li>
     {/each}
 </u1>
 
